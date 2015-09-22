d209e8b7632df58172f38777eea12e26c21231bf
Kubik2000 <jakub.drs@gmail.com>
2015-09-22 23:10:34 +0200

    Upgrade to VS2015 .net 4.6
    Recompiled in Microsoft Visual Studio 2015 Community  with .net
    framework 4.6

---
+++ PEProblem010/PEProblem010/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+/*
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ * 
+ * Using sieve of Eratosthenes just calculate the result.
+ */
+namespace PEProblem010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime StartTime = DateTime.Now;
+
+            UInt64 arraySize = 2000001;
+
+            UInt64[] numbers = new UInt64[arraySize];
+
+            for (UInt64 i = 0; i < arraySize; i++)
+            {
+                numbers[i] = i;
+            }
+
+            for (UInt64 i = 2; i < arraySize; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    for (UInt64 j = i + i; j < arraySize; j = j + i)
+                    {
+                        numbers[j] = 0;
+                    }
+                }
+            }
+
+            UInt64 result = 0;
+
+            for (UInt64 i = 2; i < arraySize; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    result += numbers[i];
+                }
+            }
+
+            TimeSpan elapsedTime = DateTime.Now - StartTime;
+
+            Console.WriteLine("result: " + result.ToString());
+            Console.WriteLine("elapsed time: " + elapsedTime.ToString("mm':'ss':'fff") + " mm:ss:fff");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PEProblem010
+PEProblem010
+============
+This is a project Euler problem number 010
+It has been writen in 
+C#
+Visual Studio Community 2015
+.net framework 4.6
+I strongly suggest to use this only once you have  finished solving the problem.
